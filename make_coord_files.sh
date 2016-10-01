2fc9e7286f55c89bae2d5d257e243451984df77a
Michael Wood-Vasey <wmwv@pitt.edu>
2016-09-30 21:57:35 -0400

    Make coordinate files for each SN.

---
+++ make_coord_files.sh
@@ -0,0 +1,6 @@
+for sn in SN2011gy SN2012fm PSNJ07250042+2347030 LSQ12fuk SN2011hb PTF11qzq PTF11mty SN2011iu PTF11owc SN2011ho PTF11qpc; do
+    echo "Name,RA,Dec" > ${sn}_ra_dec.txt;
+    echo ${sn} | awk '{printf "%s,", $1}' >> ${sn}_ra_dec.txt;
+# echo ${sn}, >> ${sn}_ra_dec.txt;
+    grep ${sn} ~/info/observed_target_info.txt | awk '{printf "skycoor -d -n 6 %s %s\n", $3, $4}' | sh | awk '{printf "%s,%s\n", $1, $2}' >> ${sn}_ra_dec.txt;
+done
