e87c9a9614d3532675a50e03b24203ea90f39ed4
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-11 16:29:39 -0500

    Add REPO definition to ingest script.

--- ingest_wiyn_dr1_images.sh
+++ ingest_wiyn_dr1_images.sh
@@ -3,4 +3,8 @@ source ~/.bashrc.lsst
 setup lsst_apps
 setup -k -r ~/local/lsst/obs_file
 
+TMP=${HOME}/tmp
+REPO=${TMP}/test_dr1
+mkdir -p ${REPO}
+
 ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
