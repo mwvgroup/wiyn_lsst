f786a2b765e8026a031462be64332207be45a6ed
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-17 14:16:42 -0800

    Adapt to WIYN+WHIRC

--- ingest_wiyn_dr1_images.sh
+++ ingest_wiyn_dr1_images.sh
@@ -1,7 +1,7 @@
 source ~/.bashrc.lsst
 
 setup lsst_apps
-setup -k -r ~/local/lsst/obs_file
+setup -k -r ~wmwv/lsst/obs_file
 
 TMP=${HOME}/tmp
 REPO=${TMP}/test_dr1
@@ -12,4 +12,5 @@ echo "lsst.obs.file.FileMapper" > ${REPO}/_mapper
 # python wiynAssembleExp.py ${DR1BASE}/stacks/PS1-12bwh_A_*[0-9].fits
 
 # ingestFiles.py ${REPO} ${DR1BASE}/stacks/PS1-12bwh_A_*.lsst.fits
-ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
+#ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
+ingestFiles.py ${REPO} ${DR1BASE}/*.lsst.fits
