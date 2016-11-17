a6727f7512361a7117b85f779332533a7e6abd46
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-17 16:41:44 -0500

    Adapt parse and run a bit from Twinkles.

--- wiyn_forcedPhotExternalCatalog.py
+++ wiyn_forcedPhotExternalCatalog.py
@@ -86,24 +86,58 @@ def test_find_science_images(name='SN2011gy', verbose=True):
     assert len(obs) >= 1
 
 
+def parse_the_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Run catalog-based forced photometry')
+#    parser.add_argument('coord_file', type=str,
+#                        help='Coordinate file with Name,RA,Dec.  Coordinates in J2000 decimal degrees.')
+    parser.add_argument('--dataset', default='calexp',
+                        help='Dataset to photometry, e.g., "calexp" or "deepDiff_differenceExp"')
+#    parser.add_argument('--run_phot', default=True, action='store_true',
+#                        help='Run actual photometry.  Turn off for testing catalog assembly.')
+#    parser.add_argument('--no_run_phot', dest='run_phot', action='store_false')
+#    parser.add_argument('--limit_n', default=None, type=int,
+#                        help='Number of images per filter to analyze.  For testing might choose to set to 10.')
+    parser.add_argument('--repo_dir', default=repo_dir,
+                        help='Butler repository to organize.')
+    parser.add_argument('--verbose', default=False, action='store_true', help='Verbose output.')
+    parser.add_argument('--debug', default=False, action='store_true', help='Debugging output.')
+
+    return parser.parse_args()
+
+
+
+def run(args):
+    lightcurve_visits = run_forced_photometry(args.coord_file, args.repo_dir, args.dataset,
+                                              LIMIT_N=args.limit_n, RUN_PHOT=args.run_phot)
+    make_catalogs(lightcurve_visits, args.repo_dir, dataset=args.dataset)
+
+
+def make_catalogs(lightcurve_visits_for_sn, repo_dir, dataset='calexp'):
+    for source_num, (name, info) in enumerate(lightcurve_visits_for_sn.items()):
+        out_file = '{}_{}_lc.fits'.format(name, dataset)
+        sn_lc = assemble_catalogs_into_lightcurve(info, repo_dir, source_num, dataset=dataset)
+        sn_lc.write(out_file, overwrite=True)
+
+
 if __name__ == "__main__":
+    args = parse_the_args()
+#    run(args)
+
     for name, sn in sn_with_dr1_templates.items():
         coord_file = '{}_ra_dec.txt'.format(name)
         out_file = '{}_lc.fits'.format(name)
-        dataset = 'calexp'
 
         print("Processing photometry for {}".format(name))
         lightcurve_fileroots_for_sn = {}
         for f in sn.keys():
 #        for f in 'H':
             lightcurve_fileroots_for_sn[f] = []
-            template_file = os.path.join(repo_dir, sn[f])
-            for science_file in find_science_images(name, f, repo_dir):
+            template_file = os.path.join(args.repo_dir, sn[f])
+            for science_file in find_science_images(name, f, args.repo_dir):
                 if science_file == template_file:
                     continue
                 science_fileroot = filename_to_fileroot(science_file)
                 lightcurve_fileroots_for_sn[f].append(science_fileroot)
-                run_forced_photometry(science_file, coord_file, repo_dir, dataset=dataset)
-
-        sn_lc = assemble_catalogs_into_lightcurve(lightcurve_fileroots_for_sn, repo_dir)
-        sn_lc.write(out_file, overwrite=True)
+                run_forced_photometry(science_file, coord_file, args.repo_dir, dataset=args.dataset)
