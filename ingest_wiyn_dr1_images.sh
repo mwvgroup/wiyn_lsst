d0c0260ed7b6840a79b76e16e284169aa98d5b7d
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-19 19:50:31 -0500

    Clean up ingest script.
    Standardize on ~/local/lsst for custom code versions
    and
    ${DR1BASE}/stacks for images

--- ingest_wiyn_dr1_images.sh
+++ ingest_wiyn_dr1_images.sh
@@ -1,16 +1,11 @@
 source ~/.bashrc.lsst
 
 setup lsst_apps
-setup -k -r ~wmwv/lsst/obs_file
+setup -k -r ~wmwv/local/lsst/obs_file
 
 TMP=${HOME}/tmp
 REPO=${TMP}/test_dr1
 mkdir -p ${REPO}
 echo "lsst.obs.file.FileMapper" > ${REPO}/_mapper
 
-# python wiynAssembleExp.py ${DR1BASE}/stacks/*[0-9].fits
-# python wiynAssembleExp.py ${DR1BASE}/stacks/PS1-12bwh_A_*[0-9].fits
-
-# ingestFiles.py ${REPO} ${DR1BASE}/stacks/PS1-12bwh_A_*.lsst.fits
-#ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
-ingestFiles.py ${REPO} ${DR1BASE}/*.lsst.fits
+ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
