9d515d7f5fb2a6708d6bf5dc312c8c359787c6b9
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-22 08:23:10 -0800

    Adapt to NERSC data locations and shell config.

--- ingest_wiyn_dr1_images.sh
+++ ingest_wiyn_dr1_images.sh
@@ -1,12 +1,9 @@
-source /global/homes/k/kap146/.bashrc.wiyn
-DR1BASE=/global/project/projectdirs/lsst/wmwv/test/DR1_images_rc3
-export PATH=$PATH:/global/homes/w/wmwv/local/bin
+source ~/.bashrc.wiyn
 
 setup lsst_apps
 setup -k -r /global/homes/w/wmwv/local/lsst/obs_file
 
-#SCRATCH=/global/cscratch1/sd/kap146
-TMP=${SCRATCH}/tmp
+TMP=${DR1BASE}/tmp
 REPO=${TMP}/test_dr1
 mkdir -p ${REPO}
 echo "lsst.obs.file.FileMapper" > ${REPO}/_mapper
