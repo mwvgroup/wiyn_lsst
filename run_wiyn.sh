234be2b8ee86284afe560b4b7003db0a50285690
kponder <kap146@pitt.edu>
2017-03-14 17:08:11 -0700

    updated paths, updated to run DR2

--- run_wiyn.sh
+++ run_wiyn.sh
@@ -1,13 +1,15 @@
-source ~/.bashrc.wiyn
+source /global/homes/k/kap146/.bashrc.wiyn
+export PATH=$PATH:/global/homes/w/wmwv/local/bin
 
 export ASTROMETRY_NET_DATA_DIR=/project/projectdirs/m1727/cat/2MASS
 
 #    --id filename='PS1-12bwh_A_J_20121028.lsst.fits' \
-WIYN=${HOME}/tmp
-REPO=${WIYN}/test_dr1
+WIYN=${SCRATCH}/tmp
+##HOME}/tmp
+REPO=${WIYN}/test_dr2
 
 processCcd.py ${REPO} \
-    @science_ids.list \
+    @science_ids_dr2.list \
     --output ${REPO} \
     -C processCcd.config \
     --clobber-config \
