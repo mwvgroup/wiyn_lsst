fd4febbdd5bab7bd72e808bf3961e217589f21c5
kponder <kap146@pitt.edu>
2017-03-14 17:03:34 -0700

    Updated to work for KAP- mostly expanding names of directories

--- ingest_wiyn_dr1_images.sh
+++ ingest_wiyn_dr1_images.sh
@@ -1,11 +1,16 @@
-source ~/.bashrc.lsst
+source /global/homes/k/kap146/.bashrc.wiyn
+DR1BASE=/global/project/projectdirs/lsst/wmwv/test/DR1_images_rc3
+export PATH=$PATH:/global/homes/w/wmwv/local/bin
 
 setup lsst_apps
-setup -k -r ~wmwv/local/lsst/obs_file
+setup -k -r /global/homes/w/wmwv/local/lsst/obs_file
 
-TMP=${HOME}/tmp
+#SCRATCH=/global/cscratch1/sd/kap146
+TMP=${SCRATCH}/tmp
 REPO=${TMP}/test_dr1
 mkdir -p ${REPO}
 echo "lsst.obs.file.FileMapper" > ${REPO}/_mapper
 
 ingestFiles.py ${REPO} ${DR1BASE}/stacks/*.lsst.fits
+
+sqlite3 ${REPO}/registry.sqlite3 "UPDATE raw SET filter = substr(fileroot, -10, 1) ;"
