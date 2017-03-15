9c3ac35cdc979798f29cd9ff1ae0cb0fb6b9ad1f
kponder <kap146@pitt.edu>
2017-03-14 17:06:27 -0700

    updated paths to directories to work for KAP"

--- make_wiyn_dr1_lsst.sh
+++ make_wiyn_dr1_lsst.sh
@@ -1,7 +1,9 @@
-source ~/.bashrc.lsst
+source /global/homes/w/wmwv/.bashrc.lsst
+DR1BASE=/global/project/projectdirs/lsst/wmwv/test/DR1_images_rc3
+export PATH=$PATH:/global/homes/w/wmwv/local/bin
 
 setup lsst_apps
-setup -k -r ~/local/lsst/obs_file
+setup -k -r /global/homes/w/wmwv/local/lsst/obs_file
 
 # Don't include .expmap.fits or previous .lsst.fits files
 #  Just the image stacks themselves, which end in YYYYMMDD.fits
