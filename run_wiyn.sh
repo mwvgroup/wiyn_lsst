93dc606c9dd7612cd24495dd3068891190f5aed6
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-13 16:00:02 -0400

    Use local Astrometry.net cat in DR1BASE.

--- run_wiyn.sh
+++ run_wiyn.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 #source /global/homes/k/kap146/.bashrc.wiyn
 #export PATH=$PATH:/global/homes/w/wmwv/local/bin
 
-export ASTROMETRY_NET_DATA_DIR=/project/projectdirs/m1727/cat/2MASS
+export ASTROMETRY_NET_DATA_DIR="${DR1BASE}/2MASS/2MASS"
 
 #    --id filename='PS1-12bwh_A_J_20121028.lsst.fits' \
 #    --id filename='SN2013cb_A_H_20130519.fits'
@@ -9,6 +11,6 @@ WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
 #    @science_ids_dr2.list \
-processCcd.py ${REPO} \
+processCcd.py "${REPO}" \
     --id field='SN2013cb' seq=A filter=H night=20130519 \
-    --output ${REPO}
+    --output "${REPO}"
