7ce7032b02ecc441514c9f857b49cea6e4230b38
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-23 16:48:55 -0400

    Run LSQ13cwp as example.

--- run_wiyn.sh
+++ run_wiyn.sh
@@ -11,12 +11,25 @@ WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
 #    @science_ids_dr2.list \
+#    --id field='SN2013cb' seq=A filter=H night=20130519 expnum=210 \
+#    --id field='iPTF13ebh' seq=A filter=H night=20131120 expnum=263 \
+#    --id field='iPTF13ebh' seq=A filter=H night=20131209 expnum=088 \
+#    --id field='iPTF13ebh' seq=A filter=H night=20131213 expnum=137 \
 processCcd.py "${REPO}" \
-    --id field='SN2013cb' seq=A filter=H night=20130519 expnum=210 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131120 expnum=335 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131209 expnum=312 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131213 expnum=274 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131111 expnum=562 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131120 expnum=425 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131209 expnum=335 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131213 expnum=299 \
     --output "${REPO}" \
     -c charImage.ref_match.matcher.maxOffsetPix=800 \
     -c charImage.ref_match.matcher.maxRotationDeg=0 \
     -c charImage.ref_match.matcher.allowedNonperpDeg=0 \
     --clobber-config \
-    --clobber-version \
-    --loglevel DEBUG
+    --clobber-version
+
+#    --loglevel DEBUG \
+#    --debug
