eaf4d63916592746a51d8acb83f9db72cfd64811
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-23 16:49:19 -0400

    Create skymap, run_coadd.sh

---
+++ run_coadd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WIYN=${DR1BASE}/tmp
+REPO=${WIYN}/test_dr1
+
+makeDiscreteSkyMap.py "${REPO}" --output "${REPO}" \
+    --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
+    --config skyMap.projection="TAN" \
+    --clobber-versions
+
+makeCoaddTempExp.py "${REPO}" --output "${REPO}" \
+    --selectId filter=H --id filter=H tract=0 patch=0,0^0,1^0,0^1,1 \
+    --config doApplyUberCal=False \
+    --clobber-versions
