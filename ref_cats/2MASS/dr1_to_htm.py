585dcc3552c9097e131dffd2af93d0d1fef4bcff
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-26 12:28:35 -0700

    Output cat in afwTable SourceCatalog.

--- ref_cats/2MASS/dr1_to_htm.py
+++ ref_cats/2MASS/dr1_to_htm.py
@@ -8,6 +8,8 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 
+import lsst.afw.table as afwTable
+
 DR1BASE = os.getenv('DR1BASE')
 
 def get_test_file(dr1base=DR1BASE):
@@ -24,6 +26,27 @@ def get_test_file(dr1base=DR1BASE):
 def read_in_data(test_file):
     """Read in test_file as a binary table FITS file."""
     return fits.getdata(test_file)
+
+
+def makeMinimalSchema(filter=None, debug=False):
+    """Make a minimal schema """
+
+    schema = afwTable.SourceTable.makeMinimalSchema()
+    schema.addField(
+        field="%s_mag" % filter,
+        type=np.float64,
+        doc="%s magnitude" % filter,
+    )
+    schema.addField(
+        field="%s_mag_sigma" % filter,
+        type=np.float64,
+        doc="%s magnitude uncertainty" % filter,
+    )
+    if debug:
+        print("SCHEMA: ")
+        print(schema)
+
+    return schema
 
 
 def convert_to_output_data(data):
@@ -47,8 +70,11 @@ def convert_to_output_data(data):
     # out_data =
 
     ref_cat_entries = np.isfinite(in_table['CAL_MAG'])
-
     out_table = in_table[ref_cat_entries]
+
+    schema = makeMinimalSchema(filter=filter)
+    out_cat = afwTable.SourceCatalog(schema)
+
     for new_name, old_name in translate.items():
         out_table.rename_column(old_name, new_name)
 
@@ -61,8 +87,12 @@ def write_out_data(out_table):
     out_table.write('test_out.fits', overwrite=True)
 
 
-if __name__ == "__main__":
+def test_example_catalog():
     test_file = get_test_file()
     data = read_in_data(test_file)
     out_data = convert_to_output_data(data)
     write_out_data(out_data)
+
+
+if __name__ == "__main__":
+    test_example_catalog()
