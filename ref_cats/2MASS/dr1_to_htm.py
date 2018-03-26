ea544b50c2ba2ea63fb346b0422bc783e78eb86d
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-25 17:55:43 -0700

    Refactor dr1_to_htm script to functions.

--- ref_cats/2MASS/dr1_to_htm.py
+++ ref_cats/2MASS/dr1_to_htm.py
@@ -8,32 +8,61 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 
-in_cat_dir = os.path.join(os.getenv('DR1BASE'), 'catalogs')
-path_regex = os.path.join(in_cat_dir, 'LSQ13cwp*')
+DR1BASE = os.getenv('DR1BASE')
 
-cat_files = glob.glob(path_regex)
+def get_test_file(dr1base=DR1BASE):
+    """Return a sample photometry catalog from SweetSpot DR1 processing"""
+    in_cat_dir = os.path.join(dr1base, 'catalogs')
+    path_regex = os.path.join(in_cat_dir, 'LSQ13cwp*')
 
-test_file = cat_files[0]
-# Read in
-data = fits.getdata(test_file)
-in_table = Table(data)
+    cat_files = glob.glob(path_regex)
 
-# Reformat
-filter = 'H'
-translate = {'coord_ra': 'RA', 'coord_dec': 'DEC',
-             '%s_mag' % filter: 'CAL_MAG'}
+    test_file = cat_files[0]
+    return test_file
 
-reverse_translate = {v: k for k, v in translate.items()}
 
-values = [v for v in translate.values()]
-# out_data =
+def read_in_data(test_file):
+    """Read in test_file as a binary table FITS file."""
+    return fits.getdata(test_file)
 
-ref_cat_entries = np.isfinite(in_table['CAL_MAG'])
 
-out_table = in_table[ref_cat_entries]
-for new_name, old_name in translate.items():
-    out_table.rename_column(old_name, new_name)
+def convert_to_output_data(data):
+    """Convert from DR1 FITS -> afw Table Source catalog
 
-# [Divide in HTM]
-# Write out
-out_table.write('test_out.fits', overwrite=True)
+    Return
+    --
+    out_table : afw Source table.
+
+    """
+    in_table = Table(data)
+
+    # Reformat
+    filter = 'H'
+    translate = {'coord_ra': 'RA', 'coord_dec': 'DEC',
+                 '%s_mag' % filter: 'CAL_MAG'}
+
+    reverse_translate = {v: k for k, v in translate.items()}
+
+    values = [v for v in translate.values()]
+    # out_data =
+
+    ref_cat_entries = np.isfinite(in_table['CAL_MAG'])
+
+    out_table = in_table[ref_cat_entries]
+    for new_name, old_name in translate.items():
+        out_table.rename_column(old_name, new_name)
+
+    return out_table
+
+
+def write_out_data(out_table):
+    # [Divide in HTM]
+    # Write out
+    out_table.write('test_out.fits', overwrite=True)
+
+
+if __name__ == "__main__":
+    test_file = get_test_file()
+    data = read_in_data(test_file)
+    out_data = convert_to_output_data(data)
+    write_out_data(out_data)
