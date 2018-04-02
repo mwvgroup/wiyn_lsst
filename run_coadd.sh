30c77db40c1c85d606588ac2a9f2ae5cb47f036d
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-02 14:03:22 -0400

    Use 'doAppend' to generate a successive sky map entry for PTF11mty
    Tract numbers are assigned in order of loading.

--- run_coadd.sh
+++ run_coadd.sh
@@ -30,22 +30,23 @@ assembleCoadd.py "${REPO}" --output "${REPO}" \
 makeDiscreteSkyMap.py "${REPO}" --output "${REPO}" \
     --id field='PTF11mty' seq=A filter=H night=20111025 expnum=218 \
     --config skyMap.projection="TAN" \
+    --config doAppend="True" \
     --clobber-versions
 
 makeCoaddTempExp.py "${REPO}" --output "${REPO}" \
-    --selectId filter=H --id field='PTF11mty' filter=H tract=0 patch=0,0 \
+    --selectId filter=H --id field='PTF11mty' filter=H tract=1 patch=0,0 \
     --config doApplyUberCal=False \
     --clobber-versions
 
 makeCoaddTempExp.py "${REPO}" --output "${REPO}" \
-    --selectId filter=J --id field='PTF11mty' filter=J tract=0 patch=0,0 \
+    --selectId filter=J --id field='PTF11mty' filter=J tract=1 patch=0,0 \
     --config doApplyUberCal=False \
     --clobber-versions
 
 assembleCoadd.py "${REPO}" --output "${REPO}" \
-    --selectId filter=H --id field='PTF11mty' filter=H tract=0 patch=0,0 \
+    --selectId filter=H --id field='PTF11mty' filter=H tract=1 patch=0,0 \
     --clobber-versions
 
 assembleCoadd.py "${REPO}" --output "${REPO}" \
-    --selectId filter=J --id field='PTF11mty' filter=J tract=0 patch=0,0 \
+    --selectId filter=J --id field='PTF11mty' filter=J tract=1 patch=0,0 \
     --clobber-versions
