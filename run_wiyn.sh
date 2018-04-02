e8e7fd906521f623016b76932b3696b1fa4caf3f
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-02 08:38:56 -0400

    processCcd PTF11mty fields.

--- run_wiyn.sh
+++ run_wiyn.sh
@@ -15,15 +15,22 @@ REPO=${WIYN}/test_dr1
 #    --id field='iPTF13ebh' seq=A filter=H night=20131120 expnum=263 \
 #    --id field='iPTF13ebh' seq=A filter=H night=20131209 expnum=088 \
 #    --id field='iPTF13ebh' seq=A filter=H night=20131213 expnum=137 \
+# processCcd.py "${REPO}" \
+#     --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
+#     --id field='LSQ13cwp' seq=A filter=H night=20131120 expnum=335 \
+#     --id field='LSQ13cwp' seq=A filter=H night=20131209 expnum=312 \
+#     --id field='LSQ13cwp' seq=A filter=H night=20131213 expnum=274 \
+#     --id field='LSQ13cwp' seq=A filter=J night=20131111 expnum=562 \
+#     --id field='LSQ13cwp' seq=A filter=J night=20131120 expnum=425 \
+#     --id field='LSQ13cwp' seq=A filter=J night=20131209 expnum=335 \
+#     --id field='LSQ13cwp' seq=A filter=J night=20131213 expnum=299 \
 processCcd.py "${REPO}" \
-    --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
-    --id field='LSQ13cwp' seq=A filter=H night=20131120 expnum=335 \
-    --id field='LSQ13cwp' seq=A filter=H night=20131209 expnum=312 \
-    --id field='LSQ13cwp' seq=A filter=H night=20131213 expnum=274 \
-    --id field='LSQ13cwp' seq=A filter=J night=20131111 expnum=562 \
-    --id field='LSQ13cwp' seq=A filter=J night=20131120 expnum=425 \
-    --id field='LSQ13cwp' seq=A filter=J night=20131209 expnum=335 \
-    --id field='LSQ13cwp' seq=A filter=J night=20131213 expnum=299 \
+    --id field='PTF11mty' seq=A filter=H night=20111025 expnum=218 \
+    --id field='PTF11mty' seq=A filter=H night=20111115 expnum=112 \
+    --id field='PTF11mty' seq=A filter=H night=20120925 expnum=81 \
+    --id field='PTF11mty' seq=A filter=J night=20111025 expnum=200 \
+    --id field='PTF11mty' seq=A filter=J night=20111115 expnum=130 \
+    --id field='PTF11mty' seq=A filter=J night=20121007 expnum=50 \
     --configfile config/processCcd.py \
     --output "${REPO}" \
     --clobber-config \
