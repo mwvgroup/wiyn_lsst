cce8ddd590ab4c24c1de9e3dffda663d62a339eb
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-03 12:22:15 -0400

    Run both LSQ13cwp and PTF11mty data.

--- run_wiyn.sh
+++ run_wiyn.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#source /global/homes/k/kap146/.bashrc.wiyn
-#export PATH=$PATH:/global/homes/w/wmwv/local/bin
-
-export ASTROMETRY_NET_DATA_DIR="${DR1BASE}/2MASS/2MASS"
-
-#    --id filename='PS1-12bwh_A_J_20121028.lsst.fits' \
-#    --id filename='SN2013cb_A_H_20130519.fits'
 WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
@@ -15,26 +8,22 @@ REPO=${WIYN}/test_dr1
 #    --id field='iPTF13ebh' seq=A filter=H night=20131120 expnum=263 \
 #    --id field='iPTF13ebh' seq=A filter=H night=20131209 expnum=088 \
 #    --id field='iPTF13ebh' seq=A filter=H night=20131213 expnum=137 \
-# processCcd.py "${REPO}" \
-#     --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
-#     --id field='LSQ13cwp' seq=A filter=H night=20131120 expnum=335 \
-#     --id field='LSQ13cwp' seq=A filter=H night=20131209 expnum=312 \
-#     --id field='LSQ13cwp' seq=A filter=H night=20131213 expnum=274 \
-#     --id field='LSQ13cwp' seq=A filter=J night=20131111 expnum=562 \
-#     --id field='LSQ13cwp' seq=A filter=J night=20131120 expnum=425 \
-#     --id field='LSQ13cwp' seq=A filter=J night=20131209 expnum=335 \
-#     --id field='LSQ13cwp' seq=A filter=J night=20131213 expnum=299 \
-processCcd.py "${REPO}" \
+processCcd.py "${REPO}" --output "${REPO}" \
+    --id field='LSQ13cwp' seq=A filter=H night=20131111 expnum=521 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131120 expnum=335 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131209 expnum=312 \
+    --id field='LSQ13cwp' seq=A filter=H night=20131213 expnum=274 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131111 expnum=562 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131120 expnum=425 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131209 expnum=335 \
+    --id field='LSQ13cwp' seq=A filter=J night=20131213 expnum=299 \
     --id field='PTF11mty' seq=A filter=H night=20111025 expnum=218 \
     --id field='PTF11mty' seq=A filter=H night=20111115 expnum=112 \
     --id field='PTF11mty' seq=A filter=H night=20120925 expnum=81 \
     --id field='PTF11mty' seq=A filter=J night=20111025 expnum=200 \
     --id field='PTF11mty' seq=A filter=J night=20111115 expnum=130 \
     --id field='PTF11mty' seq=A filter=J night=20121007 expnum=50 \
-    --configfile config/processCcd.py \
-    --output "${REPO}" \
-    --clobber-config \
-    --clobber-version
+    --configfile config/processCcd.py
 
 #    --loglevel DEBUG \
 #    --debug
