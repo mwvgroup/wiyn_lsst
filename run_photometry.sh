c8bde69cb060690297a3348dc092ee6bbc411dc7
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-03 11:46:04 -0400

    Add mergeCoaddMeasurements and forcedPhotCoadd steps.

--- run_photometry.sh
+++ run_photometry.sh
@@ -3,6 +3,8 @@
 WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
+# Should be able to replace all of this with multiBandDriver.py
+# But it was useful to go through step by step to get it all working.
 detectCoaddSources.py "${REPO}" --output "${REPO}" \
     --id field=PTF11mty filter=H \
     --id field=PTF11mty filter=J \
@@ -15,4 +17,14 @@ mergeCoaddDetections.py "${REPO}" --output "${REPO}" \
 
 measureCoaddSources.py "${REPO}" --output "${REPO}" \
     --id field=PTF11mty filter=H^J \
+    --configfile config/measureCoaddSources.py \
     --clobber-versions 
+
+mergeCoaddMeasurements.py "${REPO}" --output "${REPO}" \
+    --id field=PTF11mty filter=H^J \
+    --configfile config/mergeCoaddDetections.py \
+    --clobber-versions
+
+forcedPhotCoadd.py "${REPO}" --output "${REPO}" \
+    --id field=PTF11mty filter=H^J \
+    --clobber-versions
