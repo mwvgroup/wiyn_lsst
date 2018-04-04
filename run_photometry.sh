2eb3f4bbcb22c21051bf63e5ceef6ec6a2898a0e
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-04 10:37:02 -0400

    Generalize run_photometry to have a hardcoded FIELD
    Still hardcoded, but easier to swtich between different FIELDs

--- run_photometry.sh
+++ run_photometry.sh
@@ -3,28 +3,31 @@
 WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
+# FIELD=PTF11mty
+FIELD=LSQ13cwp
+
 # Should be able to replace all of this with multiBandDriver.py
 # But it was useful to go through step by step to get it all working.
 detectCoaddSources.py "${REPO}" --output "${REPO}" \
-    --id field=PTF11mty filter=H \
-    --id field=PTF11mty filter=J \
+    --id field="${FIELD}" filter=H \
+    --id field="${FIELD}" filter=J \
     --clobber-versions
 
 mergeCoaddDetections.py "${REPO}" --output "${REPO}" \
-    --id field=PTF11mty filter=H^J \
+    --id field="${FIELD}" filter=H^J \
     --configfile config/mergeCoaddDetections.py \
     --clobber-versions
 
 measureCoaddSources.py "${REPO}" --output "${REPO}" \
-    --id field=PTF11mty filter=H^J \
+    --id field="${FIELD}" filter=H^J \
     --configfile config/measureCoaddSources.py \
     --clobber-versions 
 
 mergeCoaddMeasurements.py "${REPO}" --output "${REPO}" \
-    --id field=PTF11mty filter=H^J \
+    --id field="${FIELD}" filter=H^J \
     --configfile config/mergeCoaddDetections.py \
     --clobber-versions
 
 forcedPhotCoadd.py "${REPO}" --output "${REPO}" \
-    --id field=PTF11mty filter=H^J \
+    --id field="${FIELD}" filter=H^J \
     --clobber-versions
