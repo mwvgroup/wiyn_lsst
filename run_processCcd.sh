6ce929ed88ceff329d500809ec3f7194304c25c0
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-04 16:35:56 -0400

    Explicitly restrict BLAS to 1 thread.
    We're parallelizing at the processCcd level.

--- run_processCcd.sh
+++ run_processCcd.sh
@@ -3,11 +3,9 @@
 WIYN=${DR1BASE}/tmp
 REPO=${WIYN}/test_dr1
 
-#    @science_ids_dr2.list \
-#    --id field='SN2013cb' seq=A filter=H night=20130519 expnum=210 \
-#    --id field='iPTF13ebh' seq=A filter=H night=20131120 expnum=263 \
-#    --id field='iPTF13ebh' seq=A filter=H night=20131209 expnum=088 \
-#    --id field='iPTF13ebh' seq=A filter=H night=20131213 expnum=137 \
+# Don't use multiple threads in BLAS, because we're parallelizing at the image level.
+export OMP_NUM_THREADS=1
+
 processCcd.py "${REPO}" --rerun processCcdOutputs \
     @dr1_dataid.list \
     -j 8 \
