753dee916599002b253687d10806751bccbd2ec2
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-05 17:29:36 -0400

    Convolved better-seeing image to template for this test.

--- test_imageDifference.sh
+++ test_imageDifference.sh
@@ -8,5 +8,6 @@ imageDifference.py ${REPO} --rerun processCcdOutputs:imdiff \
     --id field=PTF11mty seq=A filter=J night=20111115 expnum=130 \
    --templateId field=PTF11mty seq=A filter=J night=20121007 expnum=50 \
    --configfile config/imageDifference.py \
+   --config convolveTemplate=True \
    --clobber-config \
    --clobber-versions
