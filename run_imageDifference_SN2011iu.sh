37c34d4256bffe61bbed5e0d5a8a65ec107d81ac
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-09 09:34:47 -0400

    Add image subtraction scripts for each SN with refs in DR1.

---
+++ run_imageDifference_SN2011iu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+WIYN=${DR1BASE}/tmp
+REPO=${WIYN}/test_dr1
+
+imageDifference.py ${REPO} --rerun processCcdOutputs:imdiff \
+    -j 4 \
+    --id field=SN2011iu seq=A filter=H night=20111208 expnum=234 \
+    --id field=SN2011iu seq=A filter=H night=20120108 expnum=195 \
+    --templateId field=SN2011iu seq=A filter=H night=20120925 expnum=259 \
+    --configfile config/imageDifference.py \
+    --config convolveTemplate=True \
+    --clobber-config \
+    --clobber-versions \
+    --loglevel ip.diffim=DEBUG
+
+imageDifference.py ${REPO} --rerun processCcdOutputs:imdiff \
+    -j 4 \
+    --id field=SN2011iu seq=A filter=J night=20111208 expnum=252 \
+    --id field=SN2011iu seq=A filter=J night=20120108 expnum=214 \
+    --templateId field=SN2011iu seq=A filter=J night=20121001 expnum=241 \
+    --configfile config/imageDifference.py \
+    --config convolveTemplate=True \
+    --clobber-config \
+    --clobber-versions \
+    --loglevel ip.diffim=DEBUG
+
