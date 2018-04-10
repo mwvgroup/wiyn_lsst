5109b0bbfe4e4a5288e7f2803424b9cc6a5f4806
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-10 14:16:52 -0400

    Use processCcd instead of processCcdOutputs for rerun.

--- run_coadd.sh
+++ run_coadd.sh
@@ -5,18 +5,22 @@ FIELD=$2
 TRACT=$3
 SEQ=$4
 NIGHT=$5
-EXPNUM=$6
 
 PATCH=0,0
 
 COADD_RERUN=coadd
-PROCESSCCD_RERUN=processCcdOutputs
+PROCESSCCD_RERUN=processCcd
 
+# We make coadds in both J, H by default.
+# We define the DiscreteSkyMap in terms of the H filter.
+# That means that what we pass in should be in terms of the H filter.
 makeDiscreteSkyMap.py "${REPO}" --rerun "${PROCESSCCD_RERUN}":"${COADD_RERUN}" \
-    --id field="${FIELD}" seq=${SEQ} filter=H night=${NIGHT} expnum=${EXPNUM} \
+    --id field="${FIELD}" seq=${SEQ} filter=H night=${NIGHT} \
     --config skyMap.projection="TAN" \
     --clobber-versions
 
+# The following explicitly iterate between H, J
+# for the given FIELD+TRACT.
 makeCoaddTempExp.py "${REPO}" --rerun "${COADD_RERUN}" \
     --selectId filter=H --id field="${FIELD}" filter=H tract=${TRACT} patch=${PATCH} \
     --config doApplyUberCal=False
