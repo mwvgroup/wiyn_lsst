5be29c04519c50f395632463811c0a3895d6bbfa
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-18 09:43:04 -0400

    Separate out skymap to separate function.
    Making a discrete skymap links field name to tract number
    based on the order of the fields.
    The tract number for a given field will thus depend
    on how many of the fields successfully processed.
    So we only want to do this once.

---
+++ make_skymap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO=$1
+FIELD=$2
+TRACT=$3
+SEQ=$4
+FILTER=$5
+NIGHT=$6
+
+PATCH=0,0
+
+COADD_RERUN=coaddNew
+PROCESSCCD_RERUN=processCcd
+
+# We make coadds in both J, H by default.
+# We define the DiscreteSkyMap in terms of the H filter.
+# That means that what we pass in should be in terms of the H filter.
+makeDiscreteSkyMap.py "${REPO}" --rerun "${PROCESSCCD_RERUN}":"${COADD_RERUN}" \
+    --id field="${FIELD}" seq=${SEQ} filter=${FILTER} night=${NIGHT} \
+    --config skyMap.projection="TAN" \
+    --clobber-versions
