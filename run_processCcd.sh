d079f0bbdd359510d48cd003d2a57c6f2f511848
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-19 15:14:39 -0500

    Remove clobber versions, config from run_processCcd.sh

--- run_processCcd.sh
+++ run_processCcd.sh
@@ -9,9 +9,7 @@ export OMP_NUM_THREADS=1
 processCcd.py "${REPO}" --rerun processCcd \
     @dr1_dataid.list \
     -j 4 \
-    --configfile config/processCcd.py \
-    --clobber-versions \
-    --clobber-config
+    --configfile config/processCcd.py
 
 #    --loglevel DEBUG \
 #    --debug
