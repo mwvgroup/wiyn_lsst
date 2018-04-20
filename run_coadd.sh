dc8062d4b73130d315a82ea6fd63577a7da51f22
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-20 12:47:30 -0500

    Add -j 4 to coadd processing.

--- run_coadd.sh
+++ run_coadd.sh
@@ -15,15 +15,19 @@ PROCESSCCD_RERUN=processCcd
 # The following explicitly iterate between H, J
 # for the given FIELD+TRACT.
 makeCoaddTempExp.py "${REPO}" --rerun "${COADD_RERUN}" \
+    -j 4 \
     --selectId filter=H --id field="${FIELD}" filter=H tract=${TRACT} patch=${PATCH} \
     --config doApplyUberCal=False
 
 makeCoaddTempExp.py "${REPO}" --rerun "${COADD_RERUN}" \
+    -j 4 \
     --selectId filter=J --id field="${FIELD}" filter=J tract=${TRACT} patch=${PATCH} \
     --config doApplyUberCal=False
 
 assembleCoadd.py "${REPO}" --rerun "${COADD_RERUN}" \
+    -j 4 \
     --selectId filter=H --id field="${FIELD}" filter=H tract=${TRACT} patch=${PATCH}
 
 assembleCoadd.py "${REPO}" --rerun "${COADD_RERUN}" \
+    -j 4 \
     --selectId filter=J --id field="${FIELD}" filter=J tract=${TRACT} patch=${PATCH}
