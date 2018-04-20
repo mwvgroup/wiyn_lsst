24bce1ec57d9807264f5e95d5578bc8cf2a29f82
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-20 12:44:47 -0500

    Track "Too Many CR" failures through proxy of "LengthError".

--- success_processCcd.sh
+++ success_processCcd.sh
@@ -1,21 +1,30 @@
 file=processCcd.log
 
-successful=`grep Photometric ${file} | wc -l`
-failed=`grep FATAL ${file} | wc -l`
+failures=${file}.failures
 
-failed_aperture_corr=`grep FATAL ${file} | grep "Unable to measure aperture correction" | wc -l`
-failed_photocal=`grep FATAL ${file} | grep "No matches to use for photocal" | wc -l`
-failed_no_viable_psf=`grep FATAL ${file} | grep "No viable PSF candidates" | wc -l`
-failed_no_psf_candidates=`grep FATAL ${file} | grep "No objects passed our cuts for consideration as psf stars" | wc -l`
+successful=`grep Photometric ${file} | wc -l`
+grep FATAL ${file} > ${failures}
+failed=`wc -l ${failures}`
+
+failed_aperture_corr=`grep "Unable to measure aperture correction" ${failures} | wc -l`
+failed_photocal=`grep "No matches to use for photocal" ${failures} | wc -l`
+failed_no_viable_psf=`grep "No viable PSF candidates" ${failures} | wc -l`
+failed_no_psf_candidates=`grep "No objects passed our cuts for consideration as psf stars" ${failures} | wc -l`
+failed_too_many_cr=`grep "LengthError" ${failures} | wc -l`
+
+failed_unknown=`cat ${failures} | grep -v  "Unable to measure aperture correction" | grep -v  "No matches to use for photocal" | grep -v  "No viable PSF candidates" | grep -v  "No objects passed our cuts for consideration as psf stars" | grep -v "LengthError"`
 
 echo "Succeeded:  " $successful
 echo "Failed   :  " $failed
 echo "----"
-echo "  Aperture Correction:    " $failed_aperture_corr
-echo "  Photometric no matches: " $failed_photocal
+echo "  Aperture Correction:           " $failed_aperture_corr
+echo "  Photometric no matches:        " $failed_photocal
 echo "  No viable PSF candidates:      " $failed_no_viable_psf
 echo "  No passed PSF candidates:      " $failed_no_psf_candidates
+echo "  Too many CRs:                  " $failed_too_many_cr
 
+echo "Unknown failures: "
+echo ${failed_unknown}
 
 
 
