9ab6108dc15f6b00ac365997e2512be3011fe537
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-20 08:42:44 -0500

    More clearly check for 'FATAL' rather than 'dataId' in processCcd.log

--- success_processCcd.sh
+++ success_processCcd.sh
@@ -1,12 +1,12 @@
 file=processCcd.log
 
 successful=`grep Photometric ${file} | wc -l`
-failed=`grep dataId ${file} | wc -l`
+failed=`grep FATAL ${file} | wc -l`
 
-failed_aperture_corr=`grep dataId ${file} | grep "Unable to measure aperture correction" | wc -l`
-failed_photocal=`grep dataId ${file} | grep "No matches to use for photocal" | wc -l`
-failed_no_viable_psf=`grep dataId ${file} | grep "No viable PSF candidates" | wc -l`
-failed_no_psf_candidates=`grep dataId ${file} | grep "No objects passed our cuts for consideration as psf stars" | wc -l`
+failed_aperture_corr=`grep FATAL ${file} | grep "Unable to measure aperture correction" | wc -l`
+failed_photocal=`grep FATAL ${file} | grep "No matches to use for photocal" | wc -l`
+failed_no_viable_psf=`grep FATAL ${file} | grep "No viable PSF candidates" | wc -l`
+failed_no_psf_candidates=`grep FATAL ${file} | grep "No objects passed our cuts for consideration as psf stars" | wc -l`
 
 echo "Succeeded:  " $successful
 echo "Failed   :  " $failed
@@ -15,3 +15,7 @@ echo "  Aperture Correction:    " $failed_aperture_corr
 echo "  Photometric no matches: " $failed_photocal
 echo "  No viable PSF candidates:      " $failed_no_viable_psf
 echo "  No passed PSF candidates:      " $failed_no_psf_candidates
+
+
+
+
