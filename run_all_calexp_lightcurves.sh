2005bd9ef8b4fddcb0a00a6e03dc11fcd4b5f601
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-23 16:33:22 -0500

    Directly pass list of SN to sn_lightcurve.py
    Save the Python start-up time by just passing in the list of targets
    to calculate lightcurves for in a single sn_lightcurve.py call.

--- run_all_calexp_lightcurves.sh
+++ run_all_calexp_lightcurves.sh
@@ -5,6 +5,6 @@ REPO=${WIYN}/test_dr1
 
 join dr1_sn_lc.txt dr1_coadd_actual.list > dr1_sn_lc_actual.txt
 
-while read field tract seq filter night; do
-    python sn_lightcurve.py ${field}
-done < dr1_sn_lc_actual.txt
+fields=`cat dr1_sn_lc_actual.txt | cut -f 1 -d ' '`
+
+python sn_lightcurve.py $fields
